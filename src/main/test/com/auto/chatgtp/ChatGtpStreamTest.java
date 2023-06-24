a9f56550a5f17e95deb4d2ba39f9fd1518fc3f4c
wangddwei <1482466683@qq.com>
2023-06-25 02:10:48 +0800

    添加日志 测试

--- src/main/test/com/auto/chatgtp/ChatGtpStreamTest.java
+++ src/main/test/com/auto/chatgtp/ChatGtpStreamTest.java
@@ -1,10 +1,13 @@
 package com.auto.chatgtp;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ChatGtpStreamTest {
+    Logger logger = LoggerFactory.getLogger(ChatGtpStreamTest.class);
     @Test
     public void testChatGtpStream(){
-
+        logger.debug("-------");
     }
 }
